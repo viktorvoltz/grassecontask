dba07f69a91534cbdb1b6a1aa2cd8ca24c762eff
viktorvoltz <vctrchinyeaka@gmail.com>
2022-07-30 11:19:58 +0100

    reusable widget

--- lib/global_widgets/dash_board.dart
+++ lib/global_widgets/dash_board.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
 
 class DashBoard extends StatelessWidget {
-  const DashBoard({this.icon, this.name, Key? key }) : super(key: key);
-  final Icon? icon;
+  const DashBoard({this.icon, this.name, this.color, Key? key }) : super(key: key);
+  final IconData? icon;
   final String? name;
+  final Color? color;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      padding: const EdgeInsets.only(left: 20),
+      height: 40,
+      width: 50,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Icon(icon),
+          Text(name!),
+        ],
+      ),
     );
   }
 }
--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -7,10 +7,11 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        minimum: const EdgeInsets.all(16),
+        minimum: const EdgeInsets.all(20),
         child: Column(
           children: [
             Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: const [
                 Icon(Icons.notifications_none_outlined),
                 Icon(Icons.person)
